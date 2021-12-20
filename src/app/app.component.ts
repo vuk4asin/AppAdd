4d773c5e2e92c56fa51b3fe41a83a21b090fee7b
vuk4asin <vukasin.tonic@gmail.com>
2021-12-20 02:51:09 -0800

    DoradjenoDoKraja

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,27 +32,37 @@ export class AppComponent {
     console.log(this.members);
   }
 
-    generateTeams(){
+    generateTeams(){  
       if(!this.numberOfTeams || this.numberOfTeams <=0){
+        this.errorMessage="Invalid number of teams";
         return;
-      }
+      } 
 
+      if(this.members.length<this.numberOfTeams){
+        this.errorMessage="Not enough members";
+      }
+        
       const allMembers = [...this.members];
 
-      for(let i = 0 ; i < this.numberOfTeams;i++){
-      const randomIndex=Math.floor(Math.random() * allMembers.length);
+      while(allMembers.length){
+          for(let i = 0 ; i < this.numberOfTeams;i++){
+          const randomIndex=Math.floor(Math.random() * allMembers.length);
 
-      const member = allMembers.splice(randomIndex,1)[0];
+          const member = allMembers.splice(randomIndex,1)[0];
 
-      if(this.teams[i]){
-        this.teams[i].push(member);
-      }
-        else {
-          this.teams[i]=[member];
-        }
-      }
-    
-    console.log(this.teams)
+          if(!member) break;
+
+          if(this.teams[i]){
+            this.teams[i].push(member);
+          }
+            else {
+              this.teams[i]=[member];
+            }
+          }
+        
+         this.members=[];
+         this.numberOfTeams='';
+    }
   }
 }
 
